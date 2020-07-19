44ae2b42c045c88278e5ba1ae1a5076213155940
Graham <delafield@wisc.edu>
2020-07-19 09:32:12 -0500

    added docstrings and functionality

--- my_mzml.py
+++ my_mzml.py
@@ -13,18 +13,34 @@ class mzXML:
     def __repr__(self):
         return f"mzXML object instantiated from {self._file_path}"
 
-    def _precursor_to_csv(self, precursor_list, path=None):
+    def _precursor_to_csv(self, precursor_list, max_len=2000, path=None, intensities=False):
 
         """To be called from func 'get_precursors'. Returns .csv
-           file of all precursors identified in mzxml object."""
+           file of all precursors identified in mzxml object.
+           
+           args:
+                - precursor_list (type: list or dict) iterable containing 
+                  rounded precursor values. Precursor and intensities in dict.
+                - max_len (type: int) value representing number of rows returned
+                  from pandas dataframe
+                - path (type: str) path where .csv will be saved
+                - intensities (type: bool) when True, exported dataframe will
+                  contain intensity values associated with precursor masses
+        """
 
-        path = self._file_path.split(".")[0]  + "precursors.csv"
+        if path is None:
+            path = self._file_path.split(".")[0]  + "_precursors.csv"
+        else:
+            path = path + "_precursors.csv"
+
         if isinstance(precursor_list, list):
             df = pd.DataFrame(precursor_list)
         elif isinstance(precursor_list, dict):
             keys = list(precursor_list.keys())
             vals = list(precursor_list.values())
             df = pd.DataFrame({"mass":keys, "int":vals})
+
+        df = df.iloc[:max_len, :intensities+1]
         df.to_csv(path, index=False, header=False)
         print(f"...precursors.csv file created in {path}")
         return
@@ -32,14 +48,16 @@ class mzXML:
     def get_tree(self):
         return auxiliary.print_tree(next(self.data))
 
-    def get_precursors(self, decimals=3, by=None):
-        """function to pull all recognized precursor m/z
-           values with more than 1 fragment.
+    def get_precursors(self, decimals=2, by=None):
+
+        """Function to pull all recognized precursor m/z values with
+           more than 1 fragment.
            
            args: 
-           
-           decimals - number of decimal points returned from 
-           precursor mass"""
+                - decimals (type: int) number of decimal points returned
+                  from precursor mass
+                - by (type: str) structured order of precursor masses. 
+                  options ["Intensity"] """
 
         precursors = []
         intensities = []
