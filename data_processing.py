9c76f80e8b1fd871e04302c8e817fc1b57c49386
Graham Delafield <delafield@wisc.edu>
2022-10-04 10:12:56 -0500

    adding fancy dendrogram

--- data_processing.py
+++ data_processing.py
@@ -1,10 +1,7 @@
 import matplotlib.pyplot as plt
-import altair as alt
-alt.data_transformers.disable_max_rows()
-import pandas as pd 
 import numpy as np
 import os
-from scipy.ndimage import gaussian_filter
+from scipy.cluster import hierarchy
 
 
 def get_files(directory='.', exts=['.']):
@@ -90,3 +87,27 @@ def make_venn3(data_column: str, ident_column: str, dataframe):
     bc = (b & c) - abc
 
     return (len(a_only), len(b_only), len(ab), len(c_only), len(ac), len(bc), len(abc)), group_names
+
+def fancy_dendrogram(*args, **kwargs):
+    max_d = kwargs.pop('max_d', None)
+    if max_d and 'color_threshold' not in kwargs:
+        kwargs['color_threshold'] = max_d
+    annotate_above = kwargs.pop('annotate_above', 0)
+
+    ddata = hierarchy.dendrogram(*args, **kwargs)
+
+    if not kwargs.get('no_plot', False):
+        plt.title('Hierarchical Clustering Dendrogram (truncated)')
+        plt.xlabel('sample index or (cluster size)')
+        plt.ylabel('distance')
+        for i, d, c in zip(ddata['icoord'], ddata['dcoord'], ddata['color_list']):
+            x = 0.5 * sum(i[1:3])
+            y = d[1]
+            if y > annotate_above:
+                plt.plot(x, y, 'o', c=c)
+                plt.annotate("%.3g" % y, (x, y), xytext=(0, -5),
+                             textcoords='offset points',
+                             va='top', ha='center')
+        if max_d:
+            plt.axhline(y=max_d, c='k')
+    return ddata
