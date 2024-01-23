8b94748873d61cfbaf28e62a62b61d4ff558f543
Graham Delafield <graham.delafield@gmail.com>
2024-01-23 08:55:54 -0800

    add new colors

--- common_objects.py
+++ common_objects.py
@@ -8,5 +8,11 @@ alt_axis = alt.Axis(
 
 choice_colors = {
     "d_blue": "#2a385b",
-    "orange": "#c6601b"
+    "l_blue": "#576994",
+    "d_orange": "#af4a06",
+    "m_orange": "#c6601b",
+    "l_orange": "#f08137",
+    "d_gray": "#575757",
+    "m_gray": "#727272",
+    "l_gray": "#999999",
 }
