3d22522b8f4aa5e37d1337073ec8df2260cd8fa8
Graham Delafield <graham.delafield@gmail.com>
2024-02-20 08:45:13 -0800

    adding m_blue and calico colors

--- common_objects.py
+++ common_objects.py
@@ -8,11 +8,26 @@ alt_axis = alt.Axis(
 
 choice_colors = {
     "d_blue": "#2a385b",
-    "l_blue": "#576994",
+    "m_blue": "#4f618e",
+    "l_blue": "#6b7da8",
     "d_orange": "#af4a06",
     "m_orange": "#c6601b",
     "l_orange": "#f08137",
     "d_gray": "#575757",
     "m_gray": "#727272",
     "l_gray": "#999999",
+}
+
+calico_colors = {
+    "d_green":"#28A049",
+    "l_green":"#30BC5B",
+    "d_yellow":"#FF8C00",
+    "l_yellow":"#FFAA00",
+    "d_red":"#B2143D",
+    "l_red":"#D311642",
+    "d_blue":"#05B5DD",
+    "l_blue":"#38CCF7",
+    "d_gray":"#403C3C",
+    "m_gray":"#6E6D6D",
+    "l_gray":"#AAAAAA",
 }
